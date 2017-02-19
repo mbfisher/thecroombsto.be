92f617aa421f146069ac3337565daf178debb117
Mike Fisher <mike@mbfisher.com>
2017-02-19 16:47:39 +0000

    Add order of service

--- src/pages/MyInvitation.js
+++ src/pages/MyInvitation.js
@@ -7,7 +7,7 @@ export default class MyInvitation extends React.Component {
   defaultRsvp = `Tell us:
 - Who's coming?
 - Do have any dietary requirements?
-- How many times have you trumped today?`;
+- Any other relevant information you want us to know?`;
 
   constructor(props) {
     super(props);
@@ -64,7 +64,7 @@ export default class MyInvitation extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="invitation">
         <div className="banner">
           <div className="banner__text-wrapper">
             <h1>Ben and Rebecca</h1>
@@ -75,17 +75,49 @@ export default class MyInvitation extends React.Component {
           <div className="row">
             <div className="twelve columns">
               <h2>My Invitation</h2>
+            </div>
+          </div>
+
+          <div className="row order-of-service">
+            <div className="six columns">
+              <h3>Day</h3>
+              <p>Arrive at the Chapel at 1pm</p>
+              <hr/>
+              <p>Drinks reception & jazz</p>
+              <hr />
+              <p>Wedding breakfast in the Ratcliffe Room</p>
+            </div>
+
+            <div className="six columns">
+              <h3>Evening</h3>
+              <div className="evening">
+                <p>Arrive at the Ratcliffe Room at 7.30pm</p>
+                <hr />
+                <p>Canapés, drinks, casino & live music</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="twelve columns night-before">
+              <p>The night before the wedding we have booked the Mackaness Room for some informal drinks; you would be more than welcome to join us.</p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="twelve columns">
+              <h2>RSVP</h2>
+
               <p>Please use the box below to RSVP:</p>
 
-              <form className="invitation__rsvp" onSubmit={this.onSubmit}>
+              <form className="rsvp" onSubmit={this.onSubmit}>
                 <p><textarea name="rsvp" rows={10} cols={30} value={this.state.rsvp} onChange={this.onRsvpChange} placeholder={this.defaultRsvp}/></p>
                 <p><button type="submit">{this.state.loading ? <img src="/loading.svg" width="30px" style={{padding: "3px 0"}} /> : 'Submit'}</button></p>
                 {this.state.error && <p className="error">Oops, we couldn't process your RSVP! Please try again.</p>}
                 {this.state.success && <p className="success">Thanks for your RSVP! If you forgot something, just update the text and click 'Submit' again.</p>}
               </form>
 
-              <p>All wedding celebrations will take place at Rudding Park in the main hotel. The night before the wedding,
-                we have booked the Mackaness Room for some informal drinks, and you would be more than welcome to join us.</p>
+
             </div>
           </div>
         </div>
